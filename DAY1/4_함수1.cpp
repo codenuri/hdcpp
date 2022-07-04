6a705519cbb6063282e8c190ef735df9bdb8d2e2
ecourse <smkang@codenuri.co.kr>
2022-07-04 14:08:34 +0900

    Add files via upload

--- DAY1/4_함수1.cpp
+++ DAY1/4_함수1.cpp
@@ -9,7 +9,7 @@
 
 void f1(int a = 0, int b = 0, int c)     {} // error
 void f2(int a = 0, int b ,    int c = 0) {} // error
-void f2(int a    , int b = 0, int c = 0) {} // ok
+void f3(int a    , int b = 0, int c = 0) {} // ok
 
 
 void foo(int a = 0, int b = 0, int c = 0);
