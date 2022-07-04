788e939401026b22a99838993d5ef11d0bf62b6a
ecourse <smkang@codenuri.co.kr>
2022-07-04 11:14:34 +0900

    Add files via upload

---
+++ DAY1/2_입출력1.cpp
@@ -0,0 +1,30 @@
+﻿#include <cstdio>	// printf, scanf
+#include <iostream> // std::cout, std::cin
+
+int main()
+{
+	int n = 10;
+
+	// C style 입출력
+	printf("n = %d\n", n); // C style
+//	scanf("%d", &n);	   // 입력
+	scanf_s("%d", &n);		// 보안 강화 버전
+
+
+	// C++ style
+	// 특징 : %d, %f 등의 서식 문자열을 표기할 필요 없습니다.
+	// 정체 : printf 는 함수,
+	//       std::cout 은 객체(금요일날 자세히), 지금은 사용법만 알아두세요
+
+	std::cout << "n = " << n << std::endl;// std::endl 은 개행("\n")의미
+
+	// C++ 표준 입력
+	// 특징 :	1. %d 등의 서식 문자열 필요 없음
+	//			2. &n 이 아니라 n 을 전달
+	//       
+	std::cin >> n;
+}
+
+
+
+
