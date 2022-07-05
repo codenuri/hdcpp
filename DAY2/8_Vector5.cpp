70fbfbf429e0f31658aaa5e88e01e993f483fa8f
ecourse <smkang@codenuri.co.kr>
2022-07-05 15:50:25 +0900

    Add files via upload

---
+++ DAY2/8_Vector5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class vector
+{
+	int* ptr;
+	int  sz;
+public:
+	vector(int n, int value = 0)
+	{
+		sz = n;
+		ptr = new int[sz];
+
+		memset(ptr, value, sizeof(int) * sz);
+	}
+	// 생성자에서 new 등으로 메모리를 할당했다면 소멸자에서 delete 하면됩니다.
+	~vector() 
+	{
+		delete[] ptr; 
+	}
+
+};
+
+int main()
+{
+	vector v(5, 3);  // 5개를 3으로 초기화해 달라.
+}
