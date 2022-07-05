d53f648ea09ec1422ab2849992f78f8edb07c45e
ecourse <smkang@codenuri.co.kr>
2022-07-05 16:32:06 +0900

    Add files via upload

--- DAY2/8_Vector9.cpp
+++ DAY2/8_Vector9.cpp
@@ -2,56 +2,61 @@
 
 // 클래스를 만들지 말고, 클래스를 찍어낼때 사용할 "틀(템플릿)"을 제공하자.
 // 함수 뿐 아니라 "클래스도 템플릿"으로 할수 있습니다.
-template<typename T>
-class vector
+namespace util
 {
-	T* ptr;
-	int  sz;
-public:
-	vector(int n, T value = 0)
+	template<typename T>
+	class vector
 	{
-		sz = n;
-		ptr = new T[sz];
-
-		for (int i = 0; i < sz; i++)
+		T* ptr;
+		int  sz;
+	public:
+		vector(int n, T value = 0)
 		{
-			ptr[i] = value;
+			sz = n;
+			ptr = new T[sz];
+
+			for (int i = 0; i < sz; i++)
+			{
+				ptr[i] = value;
+			}
 		}
-	}
-	~vector() { delete[] ptr; }
+		~vector() { delete[] ptr; }
 
-	T& at(int idx) { return ptr[idx]; }
+		T& at(int idx) { return ptr[idx]; }
 
 
-	void resize(int newsize)
-	{
-		if (newsize > sz)
+		void resize(int newsize)
 		{
-			T* tmp = new T[newsize];
+			if (newsize > sz)
+			{
+				T* tmp = new T[newsize];
 
-			memcpy(tmp, ptr, sizeof(T) * sz);
+				memcpy(tmp, ptr, sizeof(T) * sz);
 
-			delete[] ptr;
+				delete[] ptr;
 
-			ptr = tmp;
+				ptr = tmp;
 
-			sz = newsize;
+				sz = newsize;
+			}
+			else
+			{
+			}
 		}
-		else
-		{
-		}
-	}
 
-	int size() { return sz; }
-	bool empty() { return sz == 0; }
-};
+		int size() { return sz; }
+		bool empty() { return sz == 0; }
+	};
+}
+
+
 
 int main()
 {
 //	vector v1(5, 3); // error. 클래스 템플릿은 타입인자를 반드시 
 					//         전달해야 합니다.
-	vector<int>    v1(5, 3);
-	vector<double> v2(5, 3.1); 
+	util::vector<int>    v1(5, 3);
+	util::vector<double> v2(5, 3.1);
 
 	v1.at(0) = 10;
 	v1.resize(10);
