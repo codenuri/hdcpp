c0abeecf3be36d45c732c09211c79d15955f3dd2
ecourse <smkang@codenuri.co.kr>
2022-07-05 16:06:14 +0900

    Add files via upload

--- DAY2/8_Vector5.cpp
+++ DAY2/8_Vector5.cpp
@@ -9,8 +9,12 @@ public:
 	{
 		sz = n;
 		ptr = new int[sz];
+		
+		for (int i = 0; i < sz; i++)
+		{
+			ptr[i] = value;
+		}
 
-		memset(ptr, value, sizeof(int) * sz);
 	}
 	// 생성자에서 new 등으로 메모리를 할당했다면 소멸자에서 delete 하면됩니다.
 	~vector() 
