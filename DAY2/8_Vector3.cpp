ecac4bfc0780095fa40794edb15b5ed7280b2a42
ecourse <smkang@codenuri.co.kr>
2022-07-05 15:37:36 +0900

    Add files via upload

--- DAY2/8_Vector3.cpp
+++ DAY2/8_Vector3.cpp
@@ -18,6 +18,7 @@ int main()
 
 	int n = 0;
 
+
 	while (1)
 	{
 		std::cin >> n;
@@ -27,6 +28,37 @@ int main()
 
 		score[count] = n; // 보관
 		++count;
+		
+		if (count == size)
+		{
+			// 4개 더 큰 메모리 할당
+			int* tmp = new int[size + 4];
+
+			// 기존 버퍼의 내용을 새로운 버퍼에 옮겨 담기
+			// score가 가리키는 곳의 메모리를
+			// tmp 가 가리키는 곳의 메모리 로 복사
+			memcpy(tmp, score, sizeof(int) * size);
+
+			// 기존 버퍼 제거
+			delete[] score; // score를 지우는게 아닙니다.
+							// score가 가리키는곳 제거
+
+			// score 포인터 변수가 새로운 메모리를 가리키도록
+			score = tmp;
+
+			// 마지막 남은 것은 ?
+			size = size + 4;
+
+		} // <= tmp 자체는 여기서 파괴
+	}
+
+	std::cout << "입력된 갯수    : " << count << std::endl;
+	std::cout << "할당된 버퍼크기 : " << size << std::endl;
+
+	for (int i = 0; i < count; i++)
+	{
+		std::cout << score[i] << std::endl;
 	}
 }
+// 실행해서 9개쯤 입력하고 -1 입력해 보세요
 
