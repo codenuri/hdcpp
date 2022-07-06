3ac31fbd6470bf49844e816af01a0c275041e7b3
ecourse <smkang@codenuri.co.kr>
2022-07-06 09:57:08 +0900

    Add files via upload

---
+++ DAY3/2_초기화리스트1.cpp
@@ -0,0 +1,23 @@
+﻿// 6_초기화리스트1    85page ~
+#include <iostream>
+#include <string>
+
+class Person
+{
+	std::string name;
+	int age;
+public:
+	Person(const std::string& n, int a)
+	{
+		name = n;
+		age = a;
+	}
+};
+int main()
+{
+	Person p1("kim", 10);
+}
+
+
+
+
