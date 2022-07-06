3cb34d2eb5aea15a89191a7887df3ce4ceef8aa2
ecourse <smkang@codenuri.co.kr>
2022-07-06 11:03:47 +0900

    Add files via upload

---
+++ DAY3/2_초기화리스트4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+// 선언과 구현의 분리
+class Point
+{
+	int x, y;
+public:
+	Point(int a = 0, int b = 0);
+};
+
+// 핵심 1. 초기화 리스트는 생성자 구현부(.cpp)에 있어야 합니다.
+//     2. 디폴트 인자는 선언부에만 표기하세요
+
+Point::Point(int a /*= 0*/, int b /*= 0*/) 
+	: x(a), 
+	  y(b) 
+{
+}
+
+
+
+int main()
+{
+
+}
+
+
+
+
