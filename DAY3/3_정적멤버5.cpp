cc81af242d8e4ec415424bcc31e32113f74e5b1c
ecourse <smkang@codenuri.co.kr>
2022-07-06 14:02:05 +0900

    Add files via upload

---
+++ DAY3/3_정적멤버5.cpp
@@ -0,0 +1,22 @@
+﻿// 정적멤버5,  4번 복사해오세요.
+#include <iostream>
+
+class Car
+{
+	int color;
+	static int cnt;
+public:
+	Car() { ++cnt; }
+	~Car() { --cnt; }
+
+	static int getCount() { return cnt; }
+};
+
+int Car::cnt = 0; 
+
+
+int main()
+{
+	std::cout << Car::getCount() << std::endl;
+}
+
