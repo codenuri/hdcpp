4d098677b4e51327db9eeffed9870cd1ea5e8eeb
ecourse <smkang@codenuri.co.kr>
2022-07-06 10:01:06 +0900

    Add files via upload

--- DAY3/2_초기화리스트1.cpp
+++ DAY3/2_초기화리스트1.cpp
@@ -7,12 +7,18 @@ class Person
 	std::string name;
 	int age;
 public:
-	Person(const std::string& n, int a)
+	// 멤버 초기화 리스트(member initializer list)
+	// => 생성자() 뒤에 : 을 적고 멤버 데이타를 초기화 하는 기술
+	Person(const std::string& n, int a) : name(n), age(a) // 좋은코드
 	{
-		name = n;
-		age = a;
+		// 아래 처럼 생성자 {} 안에서 멤버 데이타에 넣는것은 "좋은코드가 아닙니다"
+		// name = n;
+		// age = a;
 	}
 };
+
+
+
 int main()
 {
 	Person p1("kim", 10);
