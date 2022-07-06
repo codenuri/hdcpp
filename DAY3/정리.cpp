f193b06cde049419b093aced2d34969519551957
ecourse <smkang@codenuri.co.kr>
2022-07-06 15:54:24 +0900

    Add files via upload

---
+++ DAY3/정리.cpp
@@ -0,0 +1,34 @@
+// 정리
+
+// 1. C보다 발전된 C++ 문법들
+// => template, namespace, reference, overloading, inline
+//    new, nullptr, auto
+
+
+// 2. class 한개를 만들때 필요한 문법들
+// => 필요한 타입을 만들어서 사용하자, 그런데, 안전하고, 사용하기 쉽게만들자
+
+// => 접근 지정자 ( private vs public )
+// => 생성자, 소멸자
+// => 초기화 리스트(member initializer list)   ": x(0), y(0)"
+// 
+// => static member data, static member function 특징, 언제 사용하는가?
+
+// => const member function 
+//    => 상수객체는 상수 멤버 함수만 호출가능
+//    => 멤버데이타를 수정하지 않은 모든 멤버 함수는 "반드시!!" 상수멤버함수로
+
+// => this 개념. 멤버 함수 호출의 원리
+// => class 만들때 파일 분할(.h, .cpp)로 분리하는 방법
+
+// => 복사 생성자(금요일날..)
+
+// 3. 상속 ( 4일차 )
+// => 여러개 클래스를 가지고, 프로그램 구조 자체를 "객체지향"으로 설계 하는 방법
+
+
+// 4. std::vector, std::list 등의 표준 라이브러리 사용법.  - 5일차
+
+
+
+
