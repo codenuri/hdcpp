63f6d0f8b46656b462c62f0a0e107372c261c7c1
ecourse <smkang@codenuri.co.kr>
2022-07-06 09:16:40 +0900

    Add files via upload

--- DAY3/0_선언과구현의분리.cpp
+++ DAY3/0_선언과구현의분리.cpp
@@ -3,53 +3,15 @@
 // 어제 소스에서 vector8.cpp 소스에서
 // vector 클래스만 복사해 오세요
 
-class vector
-{
-	int* ptr;
-	int  sz;
-public:
-	vector(int n, int value = 0)
-	{
-		sz = n;
-		ptr = new int[sz];
+// class 만드는 방법
+// 1. class 선언 안에 멤버 함수의 구현까지 넣는 방법
+// 2. class 안에는 멤버 함수 선언만 넣고 멤버 함수 구현은 외부(.cpp)로 분리
 
-		for (int i = 0; i < sz; i++)
-		{
-			ptr[i] = value;
-		}
-	}
-	~vector() { delete[] ptr; }
-
-	int& at(int idx) { return ptr[idx]; }
-
-
-	void resize(int newsize)
-	{
-		if (newsize > sz)
-		{
-			int* tmp = new int[newsize];
-
-			memcpy(tmp, ptr, sizeof(int) * sz);
-
-			delete[] ptr;
-
-			ptr = tmp;
-
-			sz = newsize;
-		}
-		else
-		{
-		}
-	}
-
-	int size() { return sz; }
-	bool empty() { return sz == 0; }
-};
+#include "vector.h"
 
 int main()
 {
-	vector<int>    v1(5, 3);
-	vector<double> v2(5, 3.1);
+	vector v1(5, 3);
 
 	v1.at(0) = 10;
 	v1.resize(10);
