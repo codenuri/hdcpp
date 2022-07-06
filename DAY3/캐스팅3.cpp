5e5e173c8a4f3010ded419adde67d77dd50aebc8
ecourse <smkang@codenuri.co.kr>
2022-07-06 16:42:02 +0900

    Add files via upload

--- DAY3/캐스팅3.cpp
+++ DAY3/캐스팅3.cpp
@@ -4,14 +4,26 @@
 #include <cstdlib>
 
 // static_cast : 위험하지 않고, 꼭 필요한 캐스팅만 허용
-//				 
+//				 void* => 다른 타입* 허용
+//			     상속관계 타입 캐스팅도 허용
+//				그외는 대부분 실패
+
+// reinterpret_cast : 서로 다른 타입의 주소 캐스팅의 용도로만 사용
 
 int main()
 {
-	int* p1 = (int*)malloc(20);
-	int* p2 = static_cast<int*>(malloc(20));
+	// void* => int* 변환
+	int* p1 = (int*)malloc(20);					// ok
+	int* p2 = static_cast<int*>(malloc(20));	// ok
 
+	// int* => double* 변환
 	int n1 = 0;
-	double* p3 = (double*)&n1;
-	double* p4 = static_cast<double*>(&n1);
+	double* p3 = (double*)&n1;				// ok
+	double* p4 = static_cast<double*>(&n1);	// error
+	double* p5 = reinterpret_cast<double*>(&n1); // ok
+
+	
 }
+
+
+
