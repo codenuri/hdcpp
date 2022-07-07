e0482992b1283efea853946e5e42ea8f21f413ae
ecourse <smkang@codenuri.co.kr>
2022-07-07 14:32:42 +0900

    Add files via upload

--- DAY4/7_메뉴예제2.cpp
+++ DAY4/7_메뉴예제2.cpp
@@ -31,21 +31,35 @@ public:
 	// 팝업메뉴 선택시 해야 할일을 생각해 보세요.
 	void command()
 	{
-		int sz = v.size(); // 하위 메뉴 갯수
-
-		for (int i = 0; i < sz; i++)
+		while (1)
 		{
-			std::cout << i + 1 << ". " << v[i]->getTitle() << std::endl;
+			system("cls");  // 화면 지우기
+
+			int sz = v.size(); // 하위 메뉴 갯수
+
+			for (int i = 0; i < sz; i++)
+			{
+				std::cout << i + 1 << ". " << v[i]->getTitle() << std::endl;
+			}
+
+			std::cout << sz + 1 << ". 종료" << std::endl;
+
+			std::cout << "메뉴 선택하세요 >> ";
+
+			int cmd;
+			std::cin >> cmd;
+
+
+			if (cmd == sz + 1) // 종료 메뉴 선택
+				break;
+
+			if (cmd < 1 || cmd > sz + 1) // 잘못된 입력
+				continue ; // 다시 입력받기
+
+
+			v[cmd - 1]->command(); // 선택된 메뉴 실행
+
 		}
-
-		std::cout << sz + 1 << ". 종료" << std::endl;
-
-		std::cout << "메뉴 선택하세요 >> ";
-
-		int cmd;
-		std::cin >> cmd;
-
-		v[cmd - 1]->command(); // 선택된 메뉴 실행
 	}
 };
 
