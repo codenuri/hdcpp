869946a8e23c4dd3c95e261a6ea0053d44fd395d
ecourse <smkang@codenuri.co.kr>
2022-07-07 15:19:49 +0900

    Add files via upload

--- DAY4/7_메뉴예제4.cpp
+++ DAY4/7_메뉴예제4.cpp
@@ -83,11 +83,13 @@ int main()
 	PopupMenu* pm2 = new PopupMenu("색상 변경");
 
 	root->addMenu(pm1);
-	root->addMenu(pm2);
+//	root->addMenu(pm2);
+	pm1->addMenu(pm2);
 
 	pm1->addMenu(new MenuItem("HD",  11));
 	pm1->addMenu(new MenuItem("FHD", 12));
 	pm1->addMenu(new MenuItem("UHD", 13));
+	pm1->addMenu(new MenuItem("8K",  14));
 
 	pm2->addMenu(new MenuItem("RED",   21));
 	pm2->addMenu(new MenuItem("GREEN", 22));
@@ -96,5 +98,12 @@ int main()
 	// 위 메뉴의 구조를 잘 생각해 보세요
 	// 이제 시작하려면 어떻게 해야 하나요 ?
 	root->command();
-
 }
+// 객체지향 프로그래밍에서 "프로그램이란 ?"
+
+// 1. 객체를 생성하고
+// 2. 객체간의 관계를 설정하고 ( pm1->addMenu(메뉴아이템객체))
+// 3. 객체간의 메세지를 주고 받는 것(서로의 멤버 함수를 호출한다는 의미)
+// 이다!!
+
+// 모든 것은 객체로 구성된다.
