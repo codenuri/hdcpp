264123013d47aec2802e6d626f1e7ee3e6107390
ecourse <smkang@codenuri.co.kr>
2025-07-21 11:01:00 +0900

    Add files via upload

--- DAY1/2_가상함수2.cpp
+++ DAY1/2_가상함수2.cpp
@@ -21,6 +21,11 @@ int main()
 
 	// 아래 코드는 버그
 	Base* p = new Derived;
-	delete p;
+	delete p; // 이순간 소멸자 호출해야 합니다.
+
+	// 소멸자 호출도 결국 함수호출 입니다.
+	// p->소멸자() 가 됩니다.
+	// #1. static binding 할지 dynamic 할지 결정해야 합니다.
+	// #2. 
 
 }	
