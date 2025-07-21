745030cff0fcb6630481e2d6e12ee48f9cdf1831
ecourse <smkang@codenuri.co.kr>
2025-07-21 10:19:40 +0900

    Add files via upload

--- DAY1/QA1.cpp
+++ DAY1/QA1.cpp
@@ -3,7 +3,7 @@
 
 // class 와 struct 는 디폴트 접근지정자만 다르고 모든 것을 동일 합니다.
 struct A { int a;};
-struct B { int a;};
+struct B { int b;};
 
 // 핵심 : 다중 상속
 struct X : public A, public B
@@ -15,7 +15,7 @@ int main()
 {
 	X x;
 	A* pa = &x;
-	A* pb = &x;
+	B* pb = &x;
 
 	printf("%p\n", &x);
 	printf("%p\n", pa);
