21ec3bc00895893db099a5cf0747f12b08369b60
ecourse <smkang@codenuri.co.kr>
2025-07-21 10:31:14 +0900

    Add files via upload

--- DAY1/QA1.cpp
+++ DAY1/QA1.cpp
@@ -20,8 +20,17 @@ int main()
 //	B* pb = static_cast<B*>(&x); // &x + sizeof(A) => 1004
 
 	// 핵심 
-	B* pb = reinterpret_cast<B*>(&x); //??
+	// static_cast<B*>(&x) : &x 에서 B 의 모양을 찾아라
+	//						 반환 값은 &x 가 아닌 B의 모양이 있는 주소
+	//						 B가 없다면 관련성 없으므로 에러
 
+	// reinterpret_cast<B*>(&x) : &x 주소를 무조건 B타입으로 해석하겠다는 의도
+	//							 연관성 없어도 됩니다.
+	//							 항상 &x 주소 반환.
+	B* pb = reinterpret_cast<B*>(&x); // 1000
+
+	pb->b = 10; // x 객체의 어느 멤버가 변경될까요 ?
+				// x.a 에 10 대입!
 
 	printf("%p\n", &x); // 1000
 	printf("%p\n", pa); // &x와 동일.  
