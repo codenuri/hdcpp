49e3f0098c1f04570bc4edf6910de0deb5af882e
ecourse <smkang@codenuri.co.kr>
2025-07-21 13:03:06 +0900

    Add files via upload

---
+++ DAY1/3_도형편집기1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+
+// 파워 포인트 같은 프로그램을 객체지향으로 설계해 봅시다. - 15 page
+
+// #1. 각 도형을 타입으로 설계한다.
+
+class Rect 
+{
+	int x, y, w, h;
+public:
+	void draw() { std::cout << "draw rect\n"; }
+};
+
+int main()
+{
+
+}
+
