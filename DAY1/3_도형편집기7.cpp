4c1005ebbc2329c59eb662443a1d13e7f776c221
ecourse <smkang@codenuri.co.kr>
2025-07-21 13:48:10 +0900

    Add files via upload

---
+++ DAY1/3_도형편집기7.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+// #7.
+
+
+class Shape 
+{
+	int color = 0;
+public:
+	virtual ~Shape() {}
+
+	void set_color(int c) { color = c;}
+	virtual int get_area() {return 0;}
+
+	virtual void draw() { std::cout << "draw Shape\n"; }
+};
+
+
+
+
+
+
+class Rect : public Shape
+{
+public:
+	void draw() { std::cout << "draw rect\n"; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw circle\n"; }
+};
+class Triangle : public Shape
+{
+public:
+	void draw() { std::cout << "draw Triangle\n"; }
+};
+int main()
+{
+	std::vector<Shape*> v; 
+
+	while( true )
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if      ( cmd == 1 ) v.push_back( new Rect);
+		else if ( cmd == 2 ) v.push_back( new Circle);
+		else if ( cmd == 9 )
+		{
+			for ( auto s : v)
+			{
+				s->draw(); 
+
+			}
+		}
+		else if (cmd == 8 )
+		{
+			std::cout << "몇번째 도형을 복제할까요 >> ";
+
+			int k;
+			std::cin >> k;
+
+			// k 번째 만든 도형을 복사해서 한개 더만든후 v에 추가
+			// => 어떻게 구현해야 할까요 ?
+			// => k번째 만든 도형이 어떤 도형일까요 ?
+
+			// 방법#1. 조사 하자.
+			if ( Rect* r = dynamic_cast<Rect*>(v[k-1]); r != nullptr)
+			{				
+			}
+		}
+	}
+}
+
+
