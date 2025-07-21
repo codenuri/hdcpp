a9f437e7f735eb7ec1bcfec5b08965e14a35b4ae
ecourse <smkang@codenuri.co.kr>
2025-07-21 13:12:51 +0900

    Add files via upload

--- DAY1/3_도형편집기3.cpp
+++ DAY1/3_도형편집기3.cpp
@@ -32,7 +32,7 @@ int main()
 		std::cin >> cmd;
 
 		if      ( cmd == 1 ) v.push_back( new Rect);
-		else if ( cmd == 2 ) v.push_back( new Rect);
+		else if ( cmd == 2 ) v.push_back( new Circle);
 		else if ( cmd == 9 )
 		{
 			for ( auto s : v)
