4caa50b372c68219232b406b92980af67f57c776
ecourse <smkang@codenuri.co.kr>
2025-07-21 15:12:27 +0900

    Add files via upload

---
+++ DAY1/4_추상클래스2.cpp
@@ -0,0 +1,33 @@
+﻿//1_추상클래스2.cpp
+#include <iostream>
+
+
+class Camera
+{
+public:	
+	void take() { std::cout << "Take Picture" << std::endl; }
+};
+
+class HDCamera
+{
+public:	
+	void take() { std::cout << "Take 좋은 Picture" << std::endl; }
+};
+
+class People
+{
+public:	
+	void use_camera(Camera* p) { p->take(); }
+};
+
+int main()
+{
+	People p;
+	Camera c;
+	p.use_camera(&c);
+}
+
+
+
+
+
