fc5e50139cf2f46abe42037c2b9ae6d724ee1159
ecourse <smkang@codenuri.co.kr>
2025-07-21 10:58:50 +0900

    Add files via upload

---
+++ DAY1/2_가상함수2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+class Base
+{
+public:
+	~Base() {}
+};
+class Derived : public Base
+{
+public:
+	Derived()  { std::cout << "Derived() 자원할당" << std::endl; }
+	~Derived() { std::cout << "~Derived() 자원해지" << std::endl; }
+};
+int main()
+{
+//	Derived d; // 아무 문제 없음
+
+	// 아래 코드도 아무 문제 없음
+//	Derived* p = new Derived;
+//	delete p;
+
+	// 아래 코드는 버그
+	Base* p = new Derived;
+	delete p;
+
+}	
