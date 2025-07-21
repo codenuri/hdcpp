8263ec68b7d0738ee134fb6eac03cd0979087ba9
ecourse <smkang@codenuri.co.kr>
2025-07-21 16:11:51 +0900

    Add files via upload

---
+++ DAY1/5_Edit3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include "getch.h" 
+
+// 변하는 것을 분리하는 2가지 기술
+// #1. 변하는 것을 가상함수로 => template method
+// #2. 변하는 것을 다른 클래스로 => strategy 패턴
+
+class Edit
+{
+	std::string data;
+public:
+	std::string get_data()
+	{
+		data.clear(); 
+		while( 1 )
+		{
+			char c = _getch();
+			
+			if ( c == 13 ) break;
+			
+			if ( isdigit(c) )
+			{
+				data.push_back(c);
+				std::cout << c;
+			}			
+		}
+		std::cout << '\n';
+
+		return data;
+	}
+};
+
+int main()
+{
+	Edit e;
+	while (1)
+	{
+		std::cout << e.get_data() << std::endl;
+	}
+}
+
+
