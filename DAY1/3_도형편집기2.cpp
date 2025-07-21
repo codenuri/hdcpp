f9bed5911e7375f14f4b865acfd7d0afb96080d5
ecourse <smkang@codenuri.co.kr>
2025-07-21 13:07:50 +0900

    Add files via upload

---
+++ DAY1/3_도형편집기2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+// #2. 기반 클래스가 있다면
+// => 모든 도형의 공통의 특징을 한곳에서 관리할수 있고
+// => 모든 도형의 객체를 하나의 컨테이너에 보관 가능한다
+
+class Shape 
+{
+	int color = 0;
+public:
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() { std::cout << "draw rect\n"; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw circle\n"; }
+};
+
+int main()
+{
+	std::vector<Rect*> v; // Rect 만 보관 가능한 컨테이너
+
+}
+
