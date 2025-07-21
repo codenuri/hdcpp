41dadeb52d009bff4a6789b423b7a951bfb21c58
ecourse <smkang@codenuri.co.kr>
2025-07-21 13:19:39 +0900

    Add files via upload

---
+++ DAY1/3_도형편집기4.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+// #3-1. 해결책 #1. 캐스팅
+
+
+class Shape 
+{
+	int color = 0;
+public:
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() { std::cout << "draw rect\n"; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw circle\n"; }
+};
+
+int main()
+{
+	std::vector<Shape*> v; 
+
+	while( true )
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if      ( cmd == 1 ) v.push_back( new Rect);
+		else if ( cmd == 2 ) v.push_back( new Circle);
+		else if ( cmd == 9 )
+		{
+			for ( auto s : v)
+			{
+				/*
+				Rect* r;
+				Circle* c;
+
+				if ( r = dynamic_cast<Rect*>(s) != nullptr )
+				{
+					r->draw();
+				}
+				*/
+				// 위 코드는 C++17 에서 나온 새로운 if 문을 사용하면 편리
+				if ( Rect* r = dynamic_cast<Rect*>(s); r != nullptr )
+				{
+					r->draw();
+				}
+
+
+				s->draw(); 
+			}
+		}
+	}
+}
+
+
