a331b5e89c48546eacb0ec1005d988b12aa688e1
ecourse <smkang@codenuri.co.kr>
2025-07-22 15:57:45 +0900

    Add files via upload

--- DAY2/4_Adapter2-1.cpp
+++ DAY2/4_Adapter2-1.cpp
@@ -3,6 +3,28 @@
 // => 항상 의도를 생각하세요
 
 // ZipDecorator 복사
+class ZipDecorator : public Stream  
+{
+	Stream* origin;
+public:
+	ZipDecorator(Stream* s) : origin(s) {}
 
+	void write(const std::string& data) override 
+	{
+		auto s = "[ " + data + "] 압축됨"; 
+		origin->write(s);				   
+	}
+};
 
 // ObjectAdapter 복사
+class ObjectAdapter : public Shape      
+{
+	TextView* origin; 					  
+public:
+	ObjectAdapter(TextView* tv) : origin(tv) {}
+
+	void draw() override 
+	{
+		origin->show();
+	}
+};
