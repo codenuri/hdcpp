952e1cb238174c7a108172bcd492b053af7b6458
ecourse <smkang@codenuri.co.kr>
2025-07-22 09:58:29 +0900

    Add files via upload

--- DAY2/1_policy_base_design6.cpp
+++ DAY2/1_policy_base_design6.cpp
@@ -1,6 +1,21 @@
 #include <string>
 #include <iostream>
 
+template<typename T, // char ? wchar_t ?, utf-8....
+	     typename Trait = std::char_traits<T>, // 문자(열) 비교 정책클래스
+		 typename Alloc = std::allocator<T>>   // 메모리 할당 정책
+class basic_string 
+{
+public:
+	bool operator==(const std::basic_string& other)
+	{
+		return Trait::compare(this.buff, other.buff);
+	}
+};
+using string = std::basic_string<char>;
+using wstring = std::basic_string<wchar_t>;
+
+
 int main()
 {
 	std::string s1 = "ABCD";
