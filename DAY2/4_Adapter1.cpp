740ff6310f7272fcd369f015810d7ab253ef9d9a
ecourse <smkang@codenuri.co.kr>
2025-07-22 15:42:43 +0900

    Add files via upload

--- DAY2/4_Adapter1.cpp
+++ DAY2/4_Adapter1.cpp
@@ -38,6 +38,21 @@ public:
 // => 그런데!! 예전에 사용하던 "TextView" 의 기능이면 딱 좋을거 같다!!!
 // => 여기서 TextView 를 사용할수 있을까 ?
 
+// TextView 의 인터페이스를 변경(show=> draw 로 변경)해서
+// 도형편집기에서 사용할수 있게 하자
+
+class Text : public TextView,  // 기능 물려받고
+			 public Shape      // 도형편집기 요구사항 만족
+{
+public:
+	Text(const std::string& data) : TextView(data) {}
+
+	void draw() override 
+	{
+		?;
+	}
+};
+
 int main()
 {
 	std::vector<Shape*> v;
