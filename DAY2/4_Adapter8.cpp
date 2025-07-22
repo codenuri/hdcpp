33edd7a46f0e4682533cf541ed47731779863f04
ecourse <smkang@codenuri.co.kr>
2025-07-22 19:11:27 +0900

    Update 4_Adapter8.cpp

--- DAY2/4_Adapter8.cpp
+++ DAY2/4_Adapter8.cpp
@@ -3,25 +3,26 @@
 #include <algorithm>
 
 template<typename T>
-class take_view 
+class take_view
 {
 	T& container;
 	int cnt;
 public:
 	take_view(T& t, int c) : container(t), cnt(c) {}
 
-	auto begin() { return container.begin();}
-	auto end() { return std::next(container.begin(), cnt);}
+	auto begin() { return container.begin(); }
+	auto end() { return std::next(container.begin(), cnt); }
 };
 
-class reverse_view 
+template<typename T>
+class reverse_view
 {
 	T& container;
 public:
 	reverse_view(T& t) : container(t) {}
 
-	auto begin() { return container.rbegin();}
-	auto end() { return container.rend();}
+	auto begin() { return container.rbegin(); }
+	auto end() { return container.rend(); }
 };
 
 // 편의를 위해 reverse_view 를 만드는 함수를 제공합니다.
@@ -33,9 +34,9 @@ reverse_view<T> reverse(T& c)
 
 int main()
 {
-	std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	std::vector<int> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	for( auto e : reverse(v)) // 파이썬과 동일!
+	for (auto e : reverse(v)) // 파이썬과 동일!
 	{
 		std::cout << e << std::endl;
 	}
