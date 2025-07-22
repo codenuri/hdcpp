d0907b46a5be9704425e79b98ecd07c0c1a15544
ecourse <smkang@codenuri.co.kr>
2025-07-22 16:17:45 +0900

    Add files via upload

---
+++ DAY2/4_Adapter3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+#include <deque>
+
+// list 의 인터페이스(함수이름)을 변경해서 stack 을 만들어 봅시다.
+template<typename T>
+class stack : public std::list<T>
+{
+public:
+	void push(const T& e) {}
+	void pop() {}
+	T& top() {}
+};
+
+
+int main()
+{
+	stack<int> s;
+	s.push(10);
+}
