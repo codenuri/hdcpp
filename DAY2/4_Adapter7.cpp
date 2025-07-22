ad0eb70dabaa67b72cc468b8db3fef24eafc1f52
ecourse <smkang@codenuri.co.kr>
2025-07-22 16:42:23 +0900

    Add files via upload

--- DAY2/4_Adapter7.cpp
+++ DAY2/4_Adapter7.cpp
@@ -14,14 +14,27 @@ public:
 	auto begin() { return container.begin();}
 	auto end() { return std::next(container.begin(), cnt);}
 };
+
+class reverse_view 
+{
+	T& container;
+public:
+	reverse_view(T& t) : container(t) {}
+
+	auto begin() { return container.rbegin();}
+	auto end() { return container.rend();}
+};
+
+// 빌드시 에러나오면 -std=c++17
 int main()
 {
 	std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
 	take_view tv(v, 5);
+	reverse_view rv(v);
 
 //	for( auto e : v)
-	for( auto e : tv)
+//	for( auto e : tv)
+	for( auto e : rv)
 	{
 		std::cout << e << std::endl;
 	}
