99e16f18eaa37c6d76380362c10bebf7b4293a8b
ecourse <smkang@codenuri.co.kr>
2025-07-23 13:58:56 +0900

    Add files via upload

---
+++ DAY3/tag_dispatching3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <mutex>
+
+
+
+// 함수 오버로딩에만 사용할 타입
+// => 멤버 필요 없음. empty class
+
+class adopt_lock_t {};  // empty class
+
+adopt_lock_t adopt_lock;
+
+
+template<typename T>
+class lock_guard 
+{
+	T& mtx;
+public:	
+	lock_guard(T& m) : mtx(m) { mtx.lock();	}
+	lock_guard(T& m, adopt_lock_t) : mtx(m) { }
+
+	~lock_guard()             { mtx.unlock();}
+};
+
+std::mutex m;
+
+int main()
+{
+	if ( m.try_lock() )
+	{
+//		lock_guard<std::mutex> g(m);  // 생성자에서 lock
+//		lock_guard<std::mutex> g(m, adopt_lock); // lock 하지 말라	
+
+		std::lock_guard<std::mutex> g(m, std::adopt_lock);
+
+	}
+	else 
+	{
+
+	}
+}
