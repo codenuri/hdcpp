614f758ef2ece0ed67bb0ac0f06461433661fbd6
ecourse <smkang@codenuri.co.kr>
2025-07-23 13:28:09 +0900

    Add files via upload

---
+++ DAY3/crtp3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// 가상함수사용시
+// => 가상함수 테이블이 생성됩니다.
+// => 모든 가상함수의 주소가 메모리 어딘가 보관됩니다.
+// => 1000개 가상함수 * 32 비트 환경 => 대략 4K 메모리 오버헤드
+
+class Window
+{
+public:
+	void event_loop()
+	{
+		click();	
+
+	}
+	virtual void click() { std::cout << "Window click\n"; }
+};
+
+
+class MainWindow : public Window 
+{
+public:
+	void click() { std::cout << "MainWindow click\n"; }
+};
+
+int main()
+{
+	MainWindow w;		
+	
+	w.event_loop();
+}
