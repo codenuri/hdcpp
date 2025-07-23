0906790265008c76739ba3f5bc3f05678b650a00
ecourse <smkang@codenuri.co.kr>
2025-07-23 16:45:01 +0900

    Add files via upload

---
+++ DAY3/2_temporary2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+class Point
+{
+public:
+	int x, y;
+
+	Point(int a, int b) { std::cout << "Point()" << std::endl; }
+	~Point() { std::cout << "~Point()" << std::endl; }
+};
+
+// 임시객체와 함수 인자
+void draw_line(const Point& from, const Point& to) {}
+
+int main()
+{
+
+	std::cout << "-----" << std::endl;
+}
+
+
+
+
+
