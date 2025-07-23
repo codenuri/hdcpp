a9999379620386f700c36678f5b7be7b5a04d9fd
ecourse <smkang@codenuri.co.kr>
2025-07-23 11:18:26 +0900

    Add files via upload

--- DAY3/2_singleton6.cpp
+++ DAY3/2_singleton6.cpp
@@ -5,6 +5,16 @@
 // => 자원 해지는 함수의 마지막 부분에서 하면 안된다. => 안전하지 않다.
 // => 생성자/소멸자에 의존해라!
 
+template<typename T>
+class lock_guard 
+{
+	T& mtx;
+public:	
+	lock_guard(T& m) : mtx(m) { mtx.lock();}
+	~lock_guard()             { mtx.unlock();}
+};
+
+
 
 class Cursor
 {
@@ -17,6 +27,7 @@ private:
 	inline static std::mutex m;
 	inline static Cursor* sinstance = nullptr;
 public:
+
 	static Cursor& get_instance()
 	{
 		m.lock();
@@ -24,6 +35,9 @@ public:
 		if ( sinstance == nullptr )
 			sinstance = new Cursor;
 
+		// 여기서 예외가 발생하거나
+		// if(실패) return ; // 나중에 이런 실수를 하면
+						     // 아래 문장은 실행될수 없다.
 		m.unlock();
 
 		return *sinstance;
