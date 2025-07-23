9f182926371b0de9073ef935c781a9b4965ceac5
ecourse <smkang@codenuri.co.kr>
2025-07-23 14:29:20 +0900

    Add files via upload

---
+++ DAY3/3_flyweight2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+
+class Image
+{
+	std::string image_url;
+
+	Image(const std::string& url) : image_url(url)
+	{
+		std::cout << url << " Downloading..." << std::endl;
+	}
+
+public:	
+	void draw() { std::cout << "Draw " << image_url << std::endl; }
+
+	static Image* create(const std::string& url)
+	{
+		Image* img = new Image(url);
+		return img;
+	}
+};
+int main()
+{
+	Image* img1 = Image::create("www.naver.com/a.png");
+	img1->draw();
+
+	Image* img2 = Image::create("www.naver.com/a.png");
+	img2->draw();
+
+}
+
+
+
