36f8901afaf133f1aacb6b9f8c445c67f9d05e2f
ecourse <smkang@codenuri.co.kr>
2025-07-23 10:53:55 +0900

    Add files via upload

--- DAY3/2_singleton3.cpp
+++ DAY3/2_singleton3.cpp
@@ -10,14 +10,18 @@ private:
 	Cursor(const Cursor&) = delete;
 	Cursor& operator=(const Cursor&) = delete;	
 
-	static Cursor* sinstance;
+	// C++17 의 inline static 문법
+	// => static 멤버 데이타의 외부선언이 없어도 됩니다
+	// => 단, 자기 자신을 값으로 할때는 외부선언 필요
+	// => "inline static Cursor instance" 는 안됨
+	inline static Cursor* sinstance;
 public:
 	static Cursor& get_instance()
 	{
 		if ( sinstance == nullptr )
 			sinstance = new Cursor;
-		
-		return instance;
+
+		return *sinstance;
 	}
 };
 
