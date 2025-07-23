d6bab88fd21f101b632f63d370836e5b2bb67cc5
ecourse <smkang@codenuri.co.kr>
2025-07-23 16:47:41 +0900

    Add files via upload

--- DAY3/2_temporary2.cpp
+++ DAY3/2_temporary2.cpp
@@ -14,6 +14,14 @@ void draw_line(const Point& from, const Point& to) {}
 
 int main()
 {
+	// (1, 1) ~ (5,5) draw_line() 으로 선을 그리고 싶다.
+
+	Point p1{1, 1};
+	Point p2{5, 5};
+	draw_line(p1, p2); 
+	// => 함수 호출뒤 더이상 p1 , p2 가 필요없습니다.
+	// => 즉시 파괴될까요 ?
+
 
 	std::cout << "-----" << std::endl;
 }
