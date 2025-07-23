455846091c8a5f5380b511dbe4ca8026b701e6f2
ecourse <smkang@codenuri.co.kr>
2025-07-23 15:48:00 +0900

    Add files via upload

---
+++ DAY3/4_Factory6.cpp
@@ -0,0 +1,25 @@
+﻿// 7_객체생성정리
+class Rect
+{
+};
+
+// 객체를 만드는 방법
+
+int main()
+{
+	// #1. 
+	Rect rc1;
+
+	// #2.
+	Rect* rc2 = new Rect;
+
+	// #3. 
+	Rect* rc3 = Rect::create();
+
+	// #4.
+	Rect* rc4 = rc3->clone();
+
+	// #5. 
+	Rect* rc5 = factory.create();
+
+}
