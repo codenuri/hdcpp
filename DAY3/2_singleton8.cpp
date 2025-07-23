f70008d1144f6a14f917f908a24ea323bfb6816a
ecourse <smkang@codenuri.co.kr>
2025-07-23 13:19:02 +0900

    Add files via upload

--- DAY3/2_singleton8.cpp
+++ DAY3/2_singleton8.cpp
@@ -12,9 +12,11 @@ private:
 public:
 	// 아래 함수는 멀티스레드 안전할까요 ?
 	// godbolt.org 접속후 이코드을 윈쪽에 붙여넣기 하세요
+	// => C++11 이후 부터 안전. 어셈코드로 확인하시면 됩니다.
 	static Cursor& get_instance()
 	{
 		static Cursor instance;
+
 		return instance;
 	}
 
