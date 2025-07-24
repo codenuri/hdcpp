37749e49304b7f8fdd563872fa0a6babffa72cf4
ecourse <smkang@codenuri.co.kr>
2025-07-24 16:25:31 +0900

    Add files via upload

---
+++ DAY4/5_lambda7.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int g = 10;
+
+int main()
+{
+	int v1 = 10, v2 = 10;
+
+	// 람다표현식에서 지역변수 변경
+	auto f1 = [v1, v2](int a) { v1 = 100; return a + v1 + v2; }; 
+
+	
+	
+	//==================================================
+	class CompilerGeneratedName 
+	{
+		int v1;
+		int v2;
+	public:
+		CompilerGeneratedName(int a, int b) : v1{a}, v2{b} {}
+
+		inline auto operator()(int a) const 
+		{
+			return a + v1 + v2;
+		}
+	};
+	auto f3 = CompilerGeneratedName{v1, v2};
+
+}
+
+
+
+
+
