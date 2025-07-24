d8698cbca7b68f71a36c651d4899c5c6a0c54cb4
ecourse <smkang@codenuri.co.kr>
2025-07-24 16:28:32 +0900

    Add files via upload

--- DAY4/5_lambda7.cpp
+++ DAY4/5_lambda7.cpp
@@ -7,7 +7,12 @@ int main()
 	int v1 = 10, v2 = 10;
 
 	// 람다표현식에서 지역변수 변경
-	auto f1 = [v1, v2](int a) { v1 = 100; return a + v1 + v2; }; 
+	// => 아래 코드는 에러
+//	auto f1 = [v1, v2](int a) { v1 = 100; return a + v1 + v2; }; 
+
+	// mutable 람다 표현식
+	// => operator() 멤버 함수를 "상수함수로 하지말라"
+	auto f1 = [v1, v2](int a) mutable { v1 = 100; return a + v1 + v2; }; 
 
 	
 	
@@ -19,8 +24,11 @@ int main()
 	public:
 		CompilerGeneratedName(int a, int b) : v1{a}, v2{b} {}
 
-		inline auto operator()(int a) const 
+//		inline auto operator()(int a) const 
+		inline auto operator()(int a) 		// mutable 람다 
 		{
+			v1 = 100;				// () 함수가 상수 함수라면 에러
+									// mutable 람다는 에러 아님
 			return a + v1 + v2;
 		}
 	};
