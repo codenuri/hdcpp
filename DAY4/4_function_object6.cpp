b114f2479474cd9c9fcbc5bcd9c73f5a6dd22954
ecourse <smkang@codenuri.co.kr>
2025-07-24 15:08:17 +0900

    Add files via upload

--- DAY4/4_function_object6.cpp
+++ DAY4/4_function_object6.cpp
@@ -10,7 +10,11 @@ struct Less    { inline bool operator()(int a, int b) const { return a < b; } };
 struct Greater { inline bool operator()(int a, int b) const { return a > b; } };
 
 //----------------------------------------------------------
-void Sort(int* x, int sz , bool(*cmp)(int, int) )
+// 비교 정책을 변경할수 있는데, 비교정책의 인라인 치환도 가능한 함수
+// => "template + 함수객체" 로 만드는 기술
+// => 예전에는 C++만 가능했는데, Rust 도 거의 동일한 개념지원
+template<typename T>
+void Sort(int* x, int sz , T cmp )
 {
 	for (int i = 0; i < sz - 1; i++)
 	{
@@ -26,6 +30,6 @@ int main()
 {
 	int x[] = { 1,3,5,7,9,2,4,6,8,10 };
 
-	Less    f1; f1(5, 3);
-	Greater f2; f2(5, 3);
+	Less    f1; f1(5, 3); Sort(x, 10, f1);
+	Greater f2; f2(5, 3); Sort(x, 10, f2);
 }
---
+++ DAY4/5_lambda1.cpp
@@ -0,0 +1,40 @@
+﻿// 5_람다표현식1
+#include <iostream>
+#include <algorithm>
+#include <functional> // C++98. std::less<> 등..
+#include <vector>
+
+int main()
+{
+	std::vector<int> v = { 1,3,5,7,9,2,4,6,8,10 };
+	
+	// sort 하고 싶다면
+	// #1. 비교정책 전달안하면 오름 차순
+	std::sort(v.begin(), v.end()); 
+
+
+	// #2. 비교 정책으로 "일반함수" - 인라인 치환 안됨
+//	std::sort(v.begin(), v.end(), cmp1); 
+
+
+	// #3. 인라인치환(빠르게) 하려면 함수 객체로
+	std::less<int> f1;
+	std::greater<int> f2;
+
+	std::sort(v.begin(), v.end(), f1); 
+
+	std::sort(v.begin(), v.end(), std::less<int>());  // C++98
+	std::sort(v.begin(), v.end(), std::less<int>{});  // C++11
+	std::sort(v.begin(), v.end(), std::less<>{});     // C++17
+	std::sort(v.begin(), v.end(), std::less{});       // C++20
+
+
+	// #4. 람다 표현식 사용 - C++11
+	std::sort(v.begin(), v.end(),  [](int a, int b) { return a < b;});
+
+}
+
+
+
+
+
