ab99e916e207ab136d2f815429dc1d465fb6c048
ecourse <smkang@codenuri.co.kr>
2025-07-24 11:00:28 +0900

    Add files via upload

--- DAY4/2_move3-2.cpp
+++ DAY4/2_move3-2.cpp
@@ -26,18 +26,29 @@ public:
 		strcpy_s(name, strlen(c.name)+1, c.name);
 		std::cout << "copy\n";
 	}
-
+/*
 	Cat(Cat&& c) : age{c.age}, name{c.name}
 	{	
 		c.name = nullptr;
 		std::cout << "move\n";
 	}
+*/
 };
 
 int main()
 {
 	Cat c1{"nabi", 2};
 
-	Cat c2 = c1; 	 		// copy
-	Cat c3 = std::move(c2); // move
+	Cat c2 = c1; 	 		
+	Cat c3 = std::move(c2); 
+
+	// 핵심 : std::move() 를 사용하면
+	// #1. move 생성자가 있는 타입은 "move 생성자 호출"
+	// #2. move 생성자가 없는 타입은 "복사 생성자 호출"
+
+	// 즉, move 생성자가 없는 class 도 std::move 사용가능
+	// => 단, move 이 효과는 없고, 복사생성자 사용
+
+	// class 설계자가 move 생성자를 만들지 않아도
+	// 사용자는 std::move 사용할수 있다는것
 }
