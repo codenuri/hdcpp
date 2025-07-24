af2bb7ac940a8b9bff895108207ced4ff36234d8
ecourse <smkang@codenuri.co.kr>
2025-07-24 16:34:27 +0900

    Add files via upload

--- DAY4/5_lambda8.cpp
+++ DAY4/5_lambda8.cpp
@@ -6,14 +6,16 @@ int main()
 {
 	int v1 = 10, v2 = 10;
 
-	auto f1 = [v1, v2](int a)         { v1 = 100; return a + v1 + v2; }; 
-	auto f2 = [v1, v2](int a) mutable { v1 = 100; return a + v1 + v2; }; 
+//	auto f1 = [v1, v2](int a)         { v1 = 100; return a + v1 + v2; }; // error
+//	auto f2 = [v1, v2](int a) mutable { v1 = 100; return a + v1 + v2; }; // ok
 
+	// 레퍼런스 캡쳐
+	// v1, v2 를 복사본이 아닌 참조로 보관합니다.
+	auto f3 = [&v1, &v2](int a) { v1 = 100; return a + v1 + v2; }; 
 
-	
+	f3(0);
 
-
-
+	std::cout << v1 << std::endl; // 100s
 
 	//==================================================
 	class CompilerGeneratedName 
