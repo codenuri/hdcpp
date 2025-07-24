2f43430568bb42627143aca8a8b5a8785a462174
ecourse <smkang@codenuri.co.kr>
2025-07-24 11:21:41 +0900

    Add files via upload

--- DAY4/2_move5-2.cpp
+++ DAY4/2_move5-2.cpp
@@ -3,7 +3,18 @@
 
 // 자동 생성 규칙.
 
-// 규칙 #1. 사용자가 이동 생성자만 만들때
+// 규칙 #3. 사용자가 이동 생성자만 만들때
+// => 컴파일러는 "복사생성자" 제공 안함
+// => 복사가 필요한 코드는 에러
+
+// => 복사 생성자의 디폴트 버전이 필요하면 "= default" 로 요청
+
+// 권장
+// #1. 최선의 코드 "복사생성자/이동생성자" 모두 만들지 말라
+// #2. 만들려면 모두 만들어라
+// "all or nothing"
+
+
 
 struct Object
 {
@@ -17,15 +28,13 @@ struct Object
 		std::cout << "사용자가 만든 이동생성자\n";
 	}
 };
-
-
 int main()
 {
 	Object o1("obj1");
 	Object o2("obj2");
 
-	Object o3 = o1;				
-	Object o4 = std::move(o2);	
+	Object o3 = o1;				// 컴파일 에러	
+	Object o4 = std::move(o2); // 사용자가 만든 이동 생성자 호출
 
 	std::cout << o1.name << std::endl; 
 	std::cout << o2.name << std::endl; 
