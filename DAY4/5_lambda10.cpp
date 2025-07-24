e720d2872522950993d6ee6013fbf1181c3c2d7d
ecourse <smkang@codenuri.co.kr>
2025-07-24 16:49:10 +0900

    Add files via upload

---
+++ DAY4/5_lambda10.cpp
@@ -0,0 +1,18 @@
+int main()
+{
+	int v1 = 10;
+
+	// QA. f1는 변경할수 없으므로 const로 하면 안되나요?
+	// => 문제될때가 있습니다
+
+	const auto f1 = [](int a) { return a + v1;};
+
+	f1(10); // ok
+
+	
+	const auto f2 = [](int a) mutable { v1 = 10; return a + v1;};
+
+	f2(10); // error.
+
+	
+}
