c1fdc2b4bd31cbf0b710e205f7add2229a20877e
ecourse <smkang@codenuri.co.kr>
2025-07-24 16:32:14 +0900

    Add files via upload

---
+++ DAY4/5_lambda8.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int g = 10;
+
+int main()
+{
+	int v1 = 10, v2 = 10;
+
+	auto f1 = [v1, v2](int a)         { v1 = 100; return a + v1 + v2; }; 
+	auto f2 = [v1, v2](int a) mutable { v1 = 100; return a + v1 + v2; }; 
+
+
+	
+
+
+
+
+	//==================================================
+	class CompilerGeneratedName 
+	{
+		int v1;
+		int v2;
+	public:
+		CompilerGeneratedName(int a, int b) : v1{a}, v2{b} {}
+
+		inline auto operator()(int a) const
+		{
+			v1 = 100;				
+			return a + v1 + v2;
+		}
+	};
+	auto f3 = CompilerGeneratedName{v1, v2};
+
+}
+
+
+
+
+
