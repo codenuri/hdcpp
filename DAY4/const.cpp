0b4b19f0af6c8ae53117a509840ec5e72196f40f
ecourse <smkang@codenuri.co.kr>
2025-07-24 16:05:53 +0900

    Add files via upload

--- DAY4/const.cpp
+++ DAY4/const.cpp
@@ -6,15 +6,24 @@ public:
 	int y = 0;
 
 	void set(int a, int b) { x = a; y = b;}
-	void print() 
+
+	// 상수 멤버 함수 : 멤버 함수 뒤에 const 붙은것
+	// 특징 #1. 멤버 함수안에서 멤버 데이타 수정 안됨
+	// 특징 #2. 상수 객체는 상수 멤버 함수만 호출 가능
+	void print() const 
 	{
+		x = 10; // error
 		std::cout << x << std::endl;
 	}
 };
 int main()
 {
-	Point p;
-	p.x = 10;
-	p.set(1, 2);
-	p.print();
+	const Point p;
+
+	p.x = 10;		// error. public 이지만 p는 상수객체
+	p.set(1, 2);	// error
+	p.print();		// error
+					// 상수 멤버 함수라면 ok
+
+	// 핵심 : 상수객체는 상수 멤버함수만 호출가능하다.
 }
