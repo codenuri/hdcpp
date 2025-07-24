251521113c277f5f96d096c28a0b017f35c5cbc7
ecourse <smkang@codenuri.co.kr>
2025-07-24 11:28:05 +0900

    Add files via upload

--- DAY4/2_move5-3.cpp
+++ DAY4/2_move5-3.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 #include <cstring>
 
-// shallow copy vs deep copy
+// 클래스가 포인터 멤버 있다면
+// => 컴파일러 제공 복사 생성자는 "얕은 복사" 현상있습니다.
+// => 반드시 복사 생성자 만들어야 합니다.
+// => 이때 이동을 지원하려면 "이동생성자" 만들어야 합니다.
+// => 즉, 다 만들어야 합니다.
 
-// shallow copy
-// => 디폴트 복사 생성자가 메모리 자체를 복사하는 것이 아니라
-//    주소만 복사하는 현상
-// => 해결책 : 사용자가 복사 생성자를 만들어야 한다.
+
+// char* 대신 std::String 을 사용했다면
+// => 사용자가 자원 관리할 필요 없다.
+// => 복사/이동 모두 만들필요 없다.
+// => 아무것도 만들지 않으면 컴파일러가 모두 지원!!!
+
+// 즉, 아래 코드는 "복사 와 이동"을 지원하는 완벽한 코드!!!
+
+// 권장
+// => 자원을 직접 관리하지 말고, 자원 관리 클래스를 사용하세요
+// char* => std::string
+// int*  => std::vector<int>, smart_pointer
 
 class Cat
 {
-	char* name;
+	std::string name;
 	int   age;
 public:
-	Cat(const char* n, int a) : age(a)
+	Cat(const std::string& n, int a) : name{n}, age{a}
 	{
-		name = new char[strlen(n) + 1];
-		strcpy_s(name, strlen(n)+1, n);
-	}
-	~Cat() { delete[] name; }
-
-	Cat(const Cat& c) : age{c.age}
-	{
-		name = new char[strlen(c.name) + 1];
-		strcpy_s(name, strlen(c.name)+1, c.name);
-	}
-
-	Cat(Cat&& c) : age{c.age}, name{c.name}
-	{	
-		c.name = nullptr;
 	}
 };
 
