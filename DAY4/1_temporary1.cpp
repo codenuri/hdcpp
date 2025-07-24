b1392e25c037dbb62c204c8796ec9f7fc00a234b
ecourse <smkang@codenuri.co.kr>
2025-07-24 12:49:48 +0900

    Add files via upload

--- DAY4/1_temporary1.cpp
+++ DAY4/1_temporary1.cpp
@@ -10,7 +10,15 @@ public:
 };
 int main()
 {
-	Point pt(1, 2); 
+//	Point pt(1, 2); // named object
+					// 수명 : {}을 벗어날때 파괴
+
+
+//	Point(1, 2);	// unnamed object
+					// 수명 : 자신을 선언한 문장의 끝(;)
+
+//	Point(1,2), std::cout << "X\n";
+	Point{1,2}, std::cout << "X\n";
 
 	std::cout << "-------" << std::endl;
 }
--- DAY4/1_temporary2.cpp
+++ DAY4/1_temporary2.cpp
@@ -14,6 +14,17 @@ void draw_line(const Point& from, const Point& to) {}
 
 int main()
 {
+	// (1, 1) ~ (5,5) draw_line() 으로 선을 그리고 싶다.
+
+	Point p1{1, 1};
+	Point p2{5, 5};
+	draw_line(p1, p2); 
+	// => 함수 호출뒤 더이상 p1 , p2 가 필요없습니다.
+	// => 즉시 파괴될까요 ?
+
+	// 인자로만 사용하는 객체는 "temporary" 로 하는것이 맞습니다.!!
+	// => 가장 좋은 코드
+	draw_line(Point{1, 1}, Point{5,5});
 
 	std::cout << "-----" << std::endl;
 }
