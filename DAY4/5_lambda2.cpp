2716df5815649248b0ea4bc5d8829ec4dfc62a9c
ecourse <smkang@codenuri.co.kr>
2025-07-24 15:14:07 +0900

    Add files via upload

---
+++ DAY4/5_lambda2.cpp
@@ -0,0 +1,36 @@
+﻿// 5_람다표현식1
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+int main()
+{
+	std::vector<int> v = { 1,3,5,7,9,2,4,6,8,10 };
+
+	// 아래 코드의 원리
+	// => 람다표현식은
+	// => 함수객체를 만드는 표기법입니다.
+	// => 람다표현식의 최종결과는 "임시객체(이름없는객체)" 입니다.
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+
+
+	// 위 코드를 컴파일러가 아래 처럼 변경합니다.
+
+	class CompilerGeneratedName
+	{
+	public:
+		inline auto operator()(int a, int b) const 
+		{
+ 			return a < b;
+		}
+	};
+	std::sort(v.begin(), v.end(), CompilerGeneratedName{} );
+
+
+
+}
+
+
+
+
+
