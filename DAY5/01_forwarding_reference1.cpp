b9bdbbbd74723315c4edbe30004ec49295279824
ecourse <smkang@codenuri.co.kr>
2025-07-25 09:05:59 +0900

    Add files via upload

---
+++ DAY5/01_forwarding_reference1.cpp
@@ -0,0 +1,18 @@
+﻿// C++11 핵심 기술 : move, lambda, T&&, 완벽한 전달
+// C++14/17 : minor update
+// C++20    : major update - concept 
+// C++23    : explicit object parameter
+// C++26    : minor update
+//-------------------------------
+
+// forwarding reference 란 ?
+// => "T&&" 템플릿
+
+template<typename T> void chronomety(T&& a)
+{
+}
+
+int main()
+{
+
+}
