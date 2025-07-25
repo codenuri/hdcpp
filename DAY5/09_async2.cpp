39012d3b84f3998e04c6471736462e3bbedb350c
ecourse <smkang@codenuri.co.kr>
2025-07-25 14:58:46 +0900

    Add files via upload

--- DAY5/09_async2.cpp
+++ DAY5/09_async2.cpp
@@ -7,18 +7,16 @@ int add(int a, int b)
 {
 	std::cout << "start add\n";
 	std::this_thread::sleep_for(2s);
-
 	std::cout << "end add\n";
 	return a + b;
 }
-
 int main()
 {
-	std::future<int> ft = std::async(&add, 1, 2); 
+//	std::future<int> ft = std::async(&add, 1, 2); 
+
+	std::async(&add, 1, 2); 
 								
 	std::cout << "continue main\n";
-
-	int ret = ft.get();
-
-	std::cout << "result : " << ret << std::endl;;
+//	int ret = ft.get();
+//	std::cout << "result : " << ret << std::endl;;
 }
