4acfefa3a3a71f918f5bf94b8ecb3190510f4d70
ecourse <smkang@codenuri.co.kr>
2025-07-25 09:35:24 +0900

    Add files via upload

--- DAY5/01_forwarding_reference3-2.cpp
+++ DAY5/01_forwarding_reference3-2.cpp
@@ -1,10 +1,9 @@
 // 함수 인자로
 // int&	 : int 타입의 lvalue 만 받을수 있다.
 // int&& : int 타입의 rvalue 만 받을수 있다.
-
-// T& : 임의 타입의 lvalue 만 받을수 있다.
-//		=> T에 int&, int&& 로 결정되어도 
-//	    => int& &, int&& & 이므로 최종적으로는 lvalue reference 
+// T&    : 임의 타입의 lvalue 만 받을수 있다.
+//		  => T에 int&, int&& 로 결정되어도 
+//	      => int& &, int&& & 이므로 최종적으로는 lvalue reference 
 //--------------------------------------------------------------
 // T&& : 임의 타입의 lvalue, rvalue 모두 받을수 있다.
 
@@ -14,6 +13,12 @@
 
 // "모두 받을수 있다" 는 의미는
 // => lvalue reference 버전과 rvalue reference 버전의 함수를 "각각 생성"한다는것
+// => 생성된 함수는 "call by value" 가 아닌 "call by reference"!!
+
+// C++11 초창기에 T&& 에 대한 이름이 없었습니다.
+// => effective 책에서 "universal reference" 라는 이름 제안
+// => 그런데, 나중에 표준위원회에서 "forwarding reference" 로 결정
+//    이기술이 주로, "인자 포워딩" 에 사용되기때문에 붙인 이름
 
 template<typename T> void f4(T&& a)
 {
