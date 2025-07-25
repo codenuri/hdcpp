aa4725cb45b19556f025cdeb122d76f78fb23acc
ecourse <smkang@codenuri.co.kr>
2025-07-25 11:04:11 +0900

    Add files via upload

--- DAY5/02_perfect_forwarding8.cpp
+++ DAY5/02_perfect_forwarding8.cpp
@@ -3,16 +3,15 @@
 #include "chronometry.h"
 using namespace std::literals;
 
-void foo(int a, double d)
+int foo(int a, double d)
 {
 	std::cout << "start foo\n";
-
 	std::this_thread::sleep_for(2s); // 2초간 대기
-
 	std::cout << "end foo\n";
+	
+	return 100;
 }
-
 int main()
 {
-	chronometry(foo, 2, 2.3);
+	int ret = chronometry(foo, 2, 2.3); // foo(2, 2.3)
 }
