a59da18fe11a8cda42a8231d7d92fdd3eae3cb8a
ecourse <smkang@codenuri.co.kr>
2025-07-25 09:44:45 +0900

    Add files via upload

--- DAY5/01_forwarding_reference5.cpp
+++ DAY5/01_forwarding_reference5.cpp
@@ -3,13 +3,17 @@
 template<typename T> void foo(T&& arg)
 {
 	// 확인하는법
+	// #1. godbolt.org 에서 확인
+	// #2. 생성된 함수 이름 출력
+	std::cout << __func__ std::endl;
+	
 }
 int main()
 {
 	int n = 10;
 
-	foo(n);  // T = ?,  foo(? arg)
-	foo(10); // T = ?,  foo(? arg)
+	foo(n);  // T = int&,  foo(int& arg)
+	foo(10); // T = int,   foo(int&& arg)
 }
 
 
