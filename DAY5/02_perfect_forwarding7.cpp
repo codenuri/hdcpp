1f52b15bb14f3b1461b9e2aa1923ed5366f2572e
ecourse <smkang@codenuri.co.kr>
2025-07-25 11:36:01 +0900

    Add files via upload

--- DAY5/02_perfect_forwarding7.cpp
+++ DAY5/02_perfect_forwarding7.cpp
@@ -8,16 +8,17 @@ void f3(int a, double d, int& r) { r = 200;}
 template<typename F, typename ... T>
 void chronometry(F f, T&& ... arg)
 {
-	f( std::forward<T>(arg)... ); 	
+//	f( std::forward<T>(arg)... ); 	
+	std::invoke(f,std::forward<T>(arg)...  );
 }
 
 int main()
 {
 	int n = 3;
-	chronometry(f0); 	// f0()
-	chronometry(?);		// f1(10)	
-	chronometry(?);		// f2(10, 3.4)
-	chronometry(?);		// f3(10, 3.4, n)
+	chronometry(f0); 			// f0()
+	chronometry(f1, 10);		// f1(10)	
+	chronometry(f2, 10, 3.4);	// f2(10, 3.4)
+	chronometry(f3, 10, 3.4, n);// f3(10, 3.4, n)
 
 	std::cout << n << std::endl; // 200
 }
