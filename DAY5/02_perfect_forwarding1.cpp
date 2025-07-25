313b6e5f32dc218d14bd2cf940101ac66cc878f2
ecourse <smkang@codenuri.co.kr>
2025-07-25 10:13:46 +0900

    Add files via upload

---
+++ DAY5/02_perfect_forwarding1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+void foo(int a)  {}
+void goo(int& a) { a = 100; }
+
+// 성능을 측정하는 함수
+template<typename F, typename T>
+void chronomety(F f, T arg)
+{
+	// <========== 시간을 기록하고
+	f(arg); // <== 함수 호출
+	// <========== 수행시간 출력
+}
+
+int main()
+{
+	int n = 10;
+
+	chronomety(foo, 10); // foo(10);
+	chronomety(goo, n);  // goo(n);
+
+	std::cout << n << std::endl;
+}
