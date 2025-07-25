2edc02bfdd888ff1baf0d66d9989289b20f0c9a6
ecourse <smkang@codenuri.co.kr>
2025-07-25 10:23:33 +0900

    Add files via upload

--- DAY5/02_perfect_forwarding2.cpp
+++ DAY5/02_perfect_forwarding2.cpp
@@ -3,9 +3,20 @@
 void foo(int a)  {}
 void goo(int& a) { a = 100; }
 
+// 핵심 #1. int&, int&& 버전을 각각 제공해야 한다.
+// 핵심 #2. 현재 코드는 1개의 문제가 있습니다. 해결은 다음 소스
+
+void hoo(int&& arg) {} // 문제가 되는 경우
+
 
 template<typename F>
-void chronometry(F f, int arg)
+void chronometry(F f, int& arg)
+{
+	f(arg); 
+}
+
+template<typename F>
+void chronometry(F f, int&& arg)
 {
 	f(arg); 
 }
@@ -17,5 +28,9 @@ int main()
 	chronometry(foo, 10); 
 	chronometry(goo, n); 
 
-	std::cout << n << std::endl;
+	hoo(10);	// ok
+	chronometry(hoo, 10);  // 되야 하지만 error. 
+							// 이문제만 해결하면 됩니다.
+
+	std::cout << n << std::endl; // 10
 }
