fed80de5abea1f9990d8f5b525792c0e68d65f2a
ecourse <smkang@codenuri.co.kr>
2025-07-25 16:05:58 +0900

    Add files via upload

---
+++ DAY5/concept1.cpp
@@ -0,0 +1,19 @@
+// C++20 의 1번째로 뽑히는 기술 
+// concept1.cpp
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+void foo(T& t)
+{
+	t.resize(10);
+}
+
+int main()
+{
+	std::vector<int> v = {1,2,3,4,5};
+	
+	foo(v);
+}
+
