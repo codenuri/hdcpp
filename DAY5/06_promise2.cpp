75bf69fe0b73d8115d188bdf3cd6af838e5a8bf7
ecourse <smkang@codenuri.co.kr>
2025-07-25 14:22:08 +0900

    Add files via upload

--- DAY5/06_promise2.cpp
+++ DAY5/06_promise2.cpp
@@ -23,11 +23,15 @@ void add(int a, int b, std::promise<int>& pro)
 
 int main()
 {
-	// 핵심 1. 스레드가 아닌 직접 호출은 함수의 결과를 반환값으로 받으면 됩니다.
-	int ret1 = add1(1, 2);
+	// #2. 주스레드는 promise 객체를 만들고
+	//     미래의 결과를 담은 future 객체를 꺼내 놓습니다.
+	std::promise<int> pro;
+	std::future<int> ft = pro.get_future();
 
-	// add1 을 스레드로 수행하면 결과를 얻을수 없습니다
-	std::thread t(&add1, 1, 2);
+	// #3. 스레드 생성시 promise객체를 전달합니다.
+	std::thread t(&add, 1, 2, std::ref(pro));
+
+	std::cout << "continue main\n"
 
 	t.join();
 }
