ac15d272c65094934071b5eebdb89d504f61158e
ecourse <smkang@codenuri.co.kr>
2025-07-25 10:31:21 +0900

    Add files via upload

--- DAY5/02_perfect_forwarding3.cpp
+++ DAY5/02_perfect_forwarding3.cpp
@@ -9,13 +9,20 @@ void chronometry(F f, int& arg)
 	f(arg); 
 }
 
-// main					chronometry					hoo(int&&)
-// hoo(10)
-
+// main					chronometry					 hoo(int&&)
+// hoo(10) ==============================================> ok
+// chronometry(10) ======> int&& arg = 10;
+//						   hoo(arg) 인데, arg는 lvalue====> error	
+//						   hoo(static_cast<int&&>(arg))===> ok
 template<typename F>
 void chronometry(F f, int&& arg)
 {
-	f(arg); 
+//	f(arg); 
+
+	// 이함수의 인자는 원래 rvalue 였는데, 받으면서 이름이 생겼다(arg)
+	// 즉, lvalue 가 되었다.
+	// 원래 함수로 forwarding 할때는 다시 ralue 로 변경해서 전달
+	f(static_cast<int&&>(arg)); 
 }
 
 int main()
