af26ea0fc289c471031e6480b7ddeb4c68a6a19b
ecourse <smkang@codenuri.co.kr>
2025-07-25 09:47:53 +0900

    Add files via upload

--- DAY5/01_forwarding_reference5.cpp
+++ DAY5/01_forwarding_reference5.cpp
@@ -5,7 +5,11 @@ template<typename T> void foo(T&& arg)
 	// 확인하는법
 	// #1. godbolt.org 에서 확인
 	// #2. 생성된 함수 이름 출력
-	std::cout << __func__ << std::endl;
+	// __func__ : 함수 이름을 담으 표준 매크로
+	// __PRETTY_FUNCTION__ : 함수이름 + 인자모양 (표준 아님, g++ 전용)
+	// __FUNCSIG__         : 함수이름 + 인자모양 (표준 아님, vs 전용)
+	//std::cout << __func__ << std::endl;
+	std::cout << __PRETTY_FUNCTION__ << std::endl;
 
 }
 int main()
