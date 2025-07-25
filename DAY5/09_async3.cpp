54a858f1cc5a9009f5fdba483f961e85643ac0d6
ecourse <smkang@codenuri.co.kr>
2025-07-25 15:02:15 +0900

    Add files via upload

---
+++ DAY5/09_async3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <thread>
+#include <future>
+using namespace std::literals;
+
+int add(int a, int b)
+{
+	std::cout << "start add\n";
+	std::this_thread::sleep_for(2s);
+	std::cout << "end add\n";
+	return a + b;
+}
+int main()
+{
+	std::future<int> ft = std::async(&add, 1, 2); 
+								
+	std::cout << "continue main\n";
+	int ret = ft.get();
+	
+	std::cout << "finish main\n";
+	
+}
